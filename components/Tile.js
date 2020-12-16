59b8be27f270e09b13042024f4de9d74e05af8c0
Vitaly Liber <zenamax@gmail.com>
2020-12-16 15:21:40 +0300

    fields was added

--- components/Tile.js
+++ components/Tile.js
@@ -8,6 +8,8 @@ export default function Tile({
   bar_code,
   article_code,
   search,
+  balance,
+  price,
 }) {
   return (
     <Box borderRadius="md" border="1px" borderColor="gray.200" p={4} mb={4}>
@@ -41,6 +43,18 @@ export default function Tile({
         <WrapItem>
           <Badge variant="outline" fontSize="0.8em">
             Пост: {producer}
+          </Badge>
+        </WrapItem>
+      </Wrap>
+      <Wrap mt={2} direction="row">
+        <WrapItem>
+          <Badge colorScheme="green" variant="outline" fontSize="0.8em">
+            В наличии: {balance}
+          </Badge>
+        </WrapItem>
+        <WrapItem>
+          <Badge colorScheme="green" variant="outline" fontSize="0.8em">
+            Цена: {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(price)}
           </Badge>
         </WrapItem>
       </Wrap>
