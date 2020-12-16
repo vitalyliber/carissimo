2359ff91c6f8591fa2458550f841592eade73cbd
Vitaly Liber <zenamax@gmail.com>
2020-12-16 16:09:37 +0300

    modal window for fast editing was implemented

--- components/Tile.js
+++ components/Tile.js
@@ -1,4 +1,27 @@
-import { Box, Heading, Badge, Wrap, WrapItem } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import {
+  Box,
+  Heading,
+  Badge,
+  Wrap,
+  WrapItem,
+  useDisclosure,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { EditIcon } from "@chakra-ui/icons";
+
 import Highlighter from "react-highlight-words";
 
 export default function Tile({
@@ -11,53 +34,100 @@ export default function Tile({
   balance,
   price,
 }) {
+  const { onOpen, onClose: initialOnClose, isOpen } = useDisclosure();
+  const initialRef = useRef(null);
+  const [stateBalance, setStateBalance] = useState(balance);
+  const onClose = () => {
+    initialOnClose();
+    setStateBalance(balance);
+  };
   return (
-    <Box borderRadius="md" border="1px" borderColor="gray.200" p={4} mb={4}>
-      <Heading mb={3} size="md">
-        <Highlighter
-          highlightClassName="YourHighlightClass"
-          searchWords={search}
-          autoEscape={true}
-          textToHighlight={name}
-        />
-      </Heading>
-      <Wrap direction="row">
-        <WrapItem>
-          <Badge variant="outline" fontSize="0.8em">
-            Код: {good_code}
-          </Badge>
-        </WrapItem>
-
-        {bar_code && (
+    <>
+      <Box borderRadius="md" border="1px" borderColor="gray.200" p={4} mb={4}>
+        <Heading mb={3} size="md">
+          <Highlighter
+            highlightClassName="YourHighlightClass"
+            searchWords={search}
+            autoEscape={true}
+            textToHighlight={name}
+          />
+        </Heading>
+        <Wrap direction="row">
           <WrapItem>
             <Badge variant="outline" fontSize="0.8em">
-              Штрихкод: {bar_code}
+              Код: {good_code}
             </Badge>
           </WrapItem>
-        )}
-        <WrapItem>
-          <Badge variant="outline" fontSize="0.8em">
-            Арт: {article_code}
-          </Badge>
-        </WrapItem>
-        <WrapItem>
-          <Badge variant="outline" fontSize="0.8em">
-            Пост: {producer}
-          </Badge>
-        </WrapItem>
-      </Wrap>
-      <Wrap mt={2} direction="row">
-        <WrapItem>
-          <Badge colorScheme="green" variant="outline" fontSize="0.8em">
-            В наличии: {balance}
-          </Badge>
-        </WrapItem>
-        <WrapItem>
-          <Badge colorScheme="green" variant="outline" fontSize="0.8em">
-            Цена: {new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB' }).format(price)}
-          </Badge>
-        </WrapItem>
-      </Wrap>
-    </Box>
+
+          {bar_code && (
+            <WrapItem>
+              <Badge variant="outline" fontSize="0.8em">
+                Штрихкод: {bar_code}
+              </Badge>
+            </WrapItem>
+          )}
+          <WrapItem>
+            <Badge variant="outline" fontSize="0.8em">
+              Арт: {article_code}
+            </Badge>
+          </WrapItem>
+          <WrapItem>
+            <Badge variant="outline" fontSize="0.8em">
+              Пост: {producer}
+            </Badge>
+          </WrapItem>
+        </Wrap>
+        <Wrap mt={2} direction="row">
+          <WrapItem>
+            <Badge colorScheme="green" variant="outline" fontSize="0.8em">
+              Цена:{" "}
+              {new Intl.NumberFormat("ru-RU", {
+                style: "currency",
+                currency: "RUB",
+              }).format(price)}
+            </Badge>
+          </WrapItem>
+          <WrapItem>
+            <Badge
+              onClick={onOpen}
+              cursor="pointer"
+              colorScheme="green"
+              variant="outline"
+              fontSize="0.8em"
+            >
+              Остаток: {balance}
+              <EditIcon ml={2} />
+            </Badge>
+          </WrapItem>
+        </Wrap>
+      </Box>
+      <Modal isOpen={isOpen} onClose={onClose} initialFocusRef={initialRef}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Редактирования остатка</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <NumberInput
+              defaultValue={stateBalance}
+              min={0}
+              onChange={(value) => setStateBalance(value)}
+            >
+              <NumberInputField ref={initialRef} />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Закрыть
+            </Button>
+            <Button variant="ghost">Сохранить</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
