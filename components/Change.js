c12277ad7dba9b7eeeb23319c0d7c2d4f612ce82
Vitaly Liber <zenamax@gmail.com>
2020-12-30 00:39:43 +0500

    the names of goods to changes list was added

--- components/Change.js
+++ components/Change.js
@@ -1,37 +1,60 @@
-import { Box, Stack, Text, VStack, StackDivider } from "@chakra-ui/react";
+import {
+  Box,
+  Stack,
+  Text,
+  VStack,
+  StackDivider,
+  HStack,
+  Spacer,
+  useClipboard,
+  IconButton,
+} from "@chakra-ui/react";
+import { CheckIcon, CopyIcon } from "@chakra-ui/icons";
 import moment from "moment";
-import 'moment/locale/ru'
+import "moment/locale/ru";
 import useSWR from "swr";
-import {endpoint} from "../api/credentials";
+import { endpoint } from "../api/credentials";
 import fetcher from "../api/fetcher";
-moment.locale('ru')
+moment.locale("ru");
 
-export default function Change({ user, created_at, object_changes }) {
-  const { data } = useSWR(
-    `${endpoint}/car_goods/fields`,
-    fetcher,
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
+export default function Change({ user, created_at, object, object_changes }) {
+  const { data } = useSWR(`${endpoint}/car_goods/fields`, fetcher, {
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  const name = object?.name || object_changes?.name?.[1];
+  const { hasCopied, onCopy } = useClipboard(name);
   return (
-    <Box mb={5}>
+    <Box mb={7}>
       <Stack
         justifyContent="space-between"
         borderRadius="md"
         border="1px"
         borderColor="gray.200"
-        spacing={10}
-        direction="row"
+        spacing={1}
         pl={4}
         pr={4}
         pt={2}
         pb={2}
         mb={2}
       >
-        <Text fontWeight="bolder">{user}</Text>
-        <Text color="gray.500">{moment(created_at).fromNow()}</Text>
+        <HStack>
+          <Text fontWeight="bolder">{user}</Text>
+          <Spacer />
+          <Text color="gray.500">{moment(created_at).fromNow()}</Text>
+        </HStack>
+        <HStack>
+          <Text onClick={onCopy} color="gray.500" mt={1}>
+            {name}
+          </Text>
+          <IconButton
+            onClick={onCopy} ml={2}
+            colorScheme="teal"
+            aria-label="Скопировать название"
+            icon={hasCopied ? <CheckIcon /> : <CopyIcon />}
+            size="xs"
+          />
+        </HStack>
       </Stack>
       <VStack
         divider={<StackDivider borderColor="gray.200" />}
